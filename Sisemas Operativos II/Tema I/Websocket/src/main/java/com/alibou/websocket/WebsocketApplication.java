ac1bbf583624d58b82fbb3948368ee877d5eb557
beofalejandro <112975406+beofalejandro@users.noreply.github.com>
2024-01-31 17:52:12 -0600

    Document My code

--- Sisemas Operativos II/Tema I/Websocket/src/main/java/com/alibou/websocket/WebsocketApplication.java
+++ Sisemas Operativos II/Tema I/Websocket/src/main/java/com/alibou/websocket/WebsocketApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebsocketApplication {
 
+	// DEPLOY TH WEBSOCKET SERVER
 	public static void main(String[] args) {
 		SpringApplication.run(WebsocketApplication.class, args);
 	}
