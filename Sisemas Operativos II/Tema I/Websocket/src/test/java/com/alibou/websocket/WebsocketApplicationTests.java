5c2f09dd834ff770c6013e3e460ccc23916da293
beofalejandro <112975406+beofalejandro@users.noreply.github.com>
2024-01-31 16:25:39 -0600

    My first Websocket app
    #newbranch, #newproyect

---
+++ Sisemas Operativos II/Tema I/Websocket/src/test/java/com/alibou/websocket/WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibou.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
