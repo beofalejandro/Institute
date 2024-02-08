cd5778cf9224396f67612df7723a2df7afe0375c
Alejandro Barrientos <112975406+beofalejandro@users.noreply.github.com>
2024-02-08 15:25:24 -0600

    Fix Some wrongs Part III
    Fix next wrongs
    - Wrong Coments

--- Sisemas Operativos II/Tema I/Websocket/src/main/java/com/alibou/websocket/WebsocketApplication.java
+++ Sisemas Operativos II/Tema I/Websocket/src/main/java/com/alibou/websocket/WebsocketApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebsocketApplication {
 
-	// DEPLOY TH WEBSOCKET SERVER
+	// DEPLOY THE WEBSOCKET SERVER
 	public static void main(String[] args) {
 		SpringApplication.run(WebsocketApplication.class, args);
 	}
