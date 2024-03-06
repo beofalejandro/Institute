7be9d87174ce08ec5ca5a4eedc0bd4d8b1f342a4
Alejandro Barrientos <112975406+beofalejandro@users.noreply.github.com>
2024-03-06 15:20:50 -0600

    Fix some wrongs

--- Programacion Web/Tema I/Primera Practica/scripts/main.js
+++
@@ -1,9 +0,0 @@
-var header = document.getElementById('header');
-window.addEventListener('scroll', () => {
-    var scroll = window.scrollY
-    if (scroll > 15) {
-        header.style.backgroundColor = '#212121'
-    } else {
-        header.style.backgroundColor = 'transparent'
-    }
-});
