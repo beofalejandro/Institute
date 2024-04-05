80c9833e3660ac3379b1720d59d05a845932f562
Alejandro Barrientos <112975406+beofalejandro@users.noreply.github.com>
2024-04-05 09:50:56 -0600

    Update js strcuture

--- Programacion Web/Tema I/Primera Practica/scripts/main.js
+++ Programacion Web/Tema I/Primera Practica/scripts/main.js
@@ -6,3 +6,12 @@ sr.reveal('.other_products', {
     origin: 'bottom',
     reset: 'true'
 });
+
+document.getElementById("reservation-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    const bandName = document.getElementById("band-name").value;
+    const eventDate = document.getElementById("event-date").value;
+    const contactEmail = document.getElementById("contact-email").value;
+    const additionalComments = document.getElementById("additional-comments").value;
+    alert(`Nombre: ${bandName}\nFecha requerida: ${eventDate}\nCorreo de Contacto: ${contactEmail}\nComentarios Adicionales: ${additionalComments}`);
+});
